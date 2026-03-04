014fb68fe56e6c4d662591bee8399e68e38e8b9a
dwg245 <dwg0245@gmail.com>
2026-03-04 14:37:26 +0900

    [Feat] board_command 서버 구현
    컨트롤러에서 받던 사용자 정보 수정
    Board 엔티티에서 다른 엔티티와 관계를 맺고 있던 관계 전부 제거

---
+++ board_command/src/main/java/org/example/board_command/BoardService.java
@@ -0,0 +1,34 @@
+package org.example.board_command;
+
+import lombok.RequiredArgsConstructor;
+import org.example.board_command.model.Board;
+import org.example.board_command.model.BoardDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class BoardService {
+    private final BoardRepository boardRepository;
+
+    public BoardDto.RegRes register(Long userIdx, String userName, BoardDto.RegReq dto) {
+
+        Board entity = boardRepository.save(dto.toEntity(userIdx, userName));
+
+        return BoardDto.RegRes.from(entity);
+    }
+
+    public BoardDto.RegRes update(Long idx, BoardDto.RegReq dto) {
+        Board board = boardRepository.findById(idx).orElseThrow();
+        board.update(dto);
+
+        boardRepository.save(board);
+
+        return BoardDto.RegRes.from(board);
+    }
+
+    public void delete(Long idx) {
+        boardRepository.deleteById(idx);
+    }
+}
---
+++ board_command/src/main/java/org/example/board_command/model/Board.java
@@ -0,0 +1,31 @@
+package org.example.board_command.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Board {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idx;
+    @Column(nullable=false, length = 100)
+    private String title;
+    private String contents;
+
+    Long userIdx;
+    String userName;
+
+    public void update(BoardDto.RegReq dto) {
+        this.title = dto.getTitle();
+        this.contents = dto.getContents();
+    }
+}
