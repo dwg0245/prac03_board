142167e34652f4fe38a9e32469fd04c041e79118
dwg245 <dwg0245@gmail.com>
2026-03-05 17:21:11 +0900

    [init] reply 초기 설정

---
+++ reply_query/src/main/java/org/example/reply_query/ReplyQueryApplication.java
@@ -0,0 +1,13 @@
+package org.example.reply_query;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReplyQueryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReplyQueryApplication.class, args);
+    }
+
+}
