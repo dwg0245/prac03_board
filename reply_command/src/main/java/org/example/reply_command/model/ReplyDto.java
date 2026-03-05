3b7f85e446ad4f0f56e232e4e9cbb293bdbc1b0a
dwg245 <dwg0245@gmail.com>
2026-03-05 18:24:22 +0900

    [init] reply_command 응답 수정
    board_idx 응답 추가

--- reply_command/src/main/java/org/example/reply_command/model/ReplyDto.java
+++ reply_command/src/main/java/org/example/reply_command/model/ReplyDto.java
@@ -29,12 +29,14 @@ public class ReplyDto {
         private Long idx;
         private String contents;
         private String writer;
+        private Long board_idx;
 
         public static ReplyDto.ReplyRes from(Reply entity) {
             return ReplyRes.builder()
                     .idx(entity.getIdx())
                     .contents(entity.getContents())
                     .writer(entity.getUser_name())
+                    .board_idx(entity.getBoard_idx())
                     .build();
         }
     }
